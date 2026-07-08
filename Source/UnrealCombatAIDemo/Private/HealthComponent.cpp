758b6b70fb5d7ec696654bafcb1edbe0b6aaac8a
ISupertwister <ogz.frkn.clk.37@hotmail.com>
2026-07-08 18:40:21 +0200

    Implement reusable HealthComponent

--- Source/UnrealCombatAIDemo/Private/HealthComponent.cpp
+++ Source/UnrealCombatAIDemo/Private/HealthComponent.cpp
@@ -1,34 +1,102 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "HealthComponent.h"
+#include "Math/UnrealMathUtility.h"
 
-// Sets default values for this component's properties
 UHealthComponent::UHealthComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
-
-// Called when the game starts
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	CurrentHealth = MaxHealth;
+	bIsDead = false;
+
+	OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
 }
 
-
-// Called every frame
-void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UHealthComponent::ApplyDamage(float DamageAmount)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (bIsDead || DamageAmount <= 0.0f)
+	{
+		return;
+	}
 
-	// ...
+	CurrentHealth = FMath::Clamp(CurrentHealth - DamageAmount, 0.0f, MaxHealth);
+
+	UE_LOG(LogTemp, Log, TEXT("%s took %.1f damage. Health: %.1f / %.1f"),
+		*GetOwner()->GetName(),
+		DamageAmount,
+		CurrentHealth,
+		MaxHealth);
+
+	OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+
+	if (CurrentHealth <= 0.0f)
+	{
+		bIsDead = true;
+
+		UE_LOG(LogTemp, Warning, TEXT("%s died."), *GetOwner()->GetName());
+
+		OnDeath.Broadcast();
+	}
 }
 
+void UHealthComponent::Heal(float HealAmount)
+{
+	if (bIsDead || HealAmount <= 0.0f)
+	{
+		return;
+	}
+
+	CurrentHealth = FMath::Clamp(CurrentHealth + HealAmount, 0.0f, MaxHealth);
+
+	UE_LOG(LogTemp, Log, TEXT("%s healed %.1f. Health: %.1f / %.1f"),
+		*GetOwner()->GetName(),
+		HealAmount,
+		CurrentHealth,
+		MaxHealth);
+
+	OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+}
+
+void UHealthComponent::ResetHealth()
+{
+	CurrentHealth = MaxHealth;
+	bIsDead = false;
+
+	OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+
+	UE_LOG(LogTemp, Log, TEXT("%s health reset. Health: %.1f / %.1f"),
+		*GetOwner()->GetName(),
+		CurrentHealth,
+		MaxHealth);
+}
+
+float UHealthComponent::GetCurrentHealth() const
+{
+	return CurrentHealth;
+}
+
+float UHealthComponent::GetMaxHealth() const
+{
+	return MaxHealth;
+}
+
+float UHealthComponent::GetHealthPercent() const
+{
+	if (MaxHealth <= 0.0f)
+	{
+		return 0.0f;
+	}
+
+	return CurrentHealth / MaxHealth;
+}
+
+bool UHealthComponent::IsDead() const
+{
+	return bIsDead;
+}
--- Source/UnrealCombatAIDemo/Public/HealthComponent.h
+++ Source/UnrealCombatAIDemo/Public/HealthComponent.h
@@ -6,23 +6,54 @@
 #include "Components/ActorComponent.h"
 #include "HealthComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeath);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChanged, float, CurrentHealth, float, MaxHealth);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class UNREALCOMBATAIDEMO_API UHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
+public:
 	UHealthComponent();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Health", meta = (ClampMin = "1.0"))
+	float MaxHealth = 100.0f;
 
-		
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Health")
+	float CurrentHealth = 100.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Health")
+	bool bIsDead = false;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnDeath OnDeath;
+
+	UPROPERTY(BlueprintAssignable, Category = "Health")
+	FOnHealthChanged OnHealthChanged;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void ApplyDamage(float DamageAmount);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void Heal(float HealAmount);
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+	void ResetHealth();
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetCurrentHealth() const;
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
+	UFUNCTION(BlueprintPure, Category = "Health")
+	bool IsDead() const;
 };
